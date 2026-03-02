d9ea6c2141e9e976e678637e5ebb6ea35d209fc8
GoncaloaaF <goncaloaaf@gmail.com>
2026-03-02 12:56:27 +0000

    demo classes

--- Droid/07_Classes/src/Carro.kt
+++ Droid/07_Classes/src/Carro.kt
@@ -18,14 +18,30 @@ class Carro {
 class Carro2(
     var marca: String,
     var modelo: String,
-    var preco: Double
+    private var preco: Double
 ) {
     init {
-        
+        require(preco > 0) { "Preço Invalido" }
+    }
+
+    constructor(marca: String, modelo: String): this(marca, modelo, 25000.0)
+
+
+    fun funcClass(){
+        println("a minha func da data class")
     }
 
     override fun toString(): String {
-        return "marca: $marca, modelo: $modelo"
+        return "marca: $marca, modelo: $modelo, preco: $preco"
+    }
+}
+
+data class Carro3( var marca: String,
+                   private var modelo: String){
+
+    fun func_dataClass(){
+        println("a minha func da data class")
+        this.modelo = "sem modelo 2"
     }
 
 }
--- Droid/07_Classes/src/Main.kt
+++ Droid/07_Classes/src/Main.kt
@@ -32,14 +32,64 @@ fun main() {
     println(c == c3)
 
 
-    val c4 = Carro2("Audi", "R8", -31.0)
+    val c4 = Carro2("Audi", "R8", 21331.0)
+
+    println(c4)
+
+    val c5 = Carro2("Audi", "R8")
+
+    println(c5)
+
+
+    println("-------------------")
+
+
+    var c6 = Carro3("Audi", "R8")
+
+    println(c6)
+
+    c6.func_dataClass()
+
+
+    c6.marca = "BMW"
+
+    println(c6)
+    //c6.modelo = "R8"
 
 
 
+    println("-------------------")
 
 
+    var c7 = Carro3("Audi", "R8")
+    var c8 = c7
+
+    println("c7 : $c7")
+    println("c8 : $c8")
 
 
+    c8.marca = "BMW"
 
+    println("c8 : $c8")
+    println("c7 : $c7")
+
+    println("-------------------")
+
+    var c9 = Carro3("Audi", "R8")
+    var c10 = Carro3("Audi", "R8")
+
+    println(c9 == c10)
+
+    println(c8 == c7)
+
+    println("-------------------")
+
+    val p1 = Pessoa("Maria", 2000)
+
+
+    println(p1.idade)
+    p1.idade = 123
+
+    println(p1.idade)
 
 }
