d11463472de3d99ee205c44ac99869a449774e62
GoncaloaaF <goncaloaaf@gmail.com>
2026-03-31 14:25:36 +0100

    demo slider

--- Droid/11_MVVM/app/src/main/java/pt/cesae/a11_mvvm/MainActivity.kt
+++ Droid/11_MVVM/app/src/main/java/pt/cesae/a11_mvvm/MainActivity.kt
@@ -12,7 +12,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -24,6 +27,7 @@ import pt.cesae.a11_mvvm.ui.theme._11_MVVMTheme
 import kotlin.properties.ReadOnlyProperty
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -51,6 +55,7 @@ fun MainPage(viewModel: MainViewModel){
     val aluno by viewModel.aluno.collectAsStateWithLifecycle()
 
     var novoNome by remember { mutableStateOf("") }
+    var sliderVal by remember { mutableFloatStateOf(0f) }
 
     Column(horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -64,7 +69,7 @@ fun MainPage(viewModel: MainViewModel){
 
         TextField(value = novoNome,
             onValueChange = {
-                
+
                 novoNome = it
             },
             placeholder = {
@@ -74,6 +79,26 @@ fun MainPage(viewModel: MainViewModel){
                 .padding(25.dp)
 
         )
+
+        Text("$sliderVal")
+        Slider(
+            value = sliderVal,
+            onValueChange = {
+                sliderVal = it
+            },
+            colors = SliderDefaults.colors(
+                thumbColor = MaterialTheme.colorScheme.secondary,
+                activeTrackColor = MaterialTheme.colorScheme.secondary,
+                inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+            ),
+            steps = 255,
+            valueRange = 0f..255f,
+            modifier = Modifier
+                .padding(25.dp)
+
+        )
+
+        
         Button({
             //viewModel.txt.value = "Novo Valor"
             if (novoNome.length > 3){
