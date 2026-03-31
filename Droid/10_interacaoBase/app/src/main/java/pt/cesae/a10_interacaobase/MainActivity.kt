c2791a853f32a57d56e64647ad18b7f6e94f1359
GoncaloaaF <goncaloaaf@gmail.com>
2026-03-31 09:55:42 +0100

    deno TextField

--- Droid/10_interacaoBase/app/src/main/java/pt/cesae/a10_interacaobase/MainActivity.kt
+++ Droid/10_interacaoBase/app/src/main/java/pt/cesae/a10_interacaobase/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,8 +32,13 @@ class MainActivity : ComponentActivity() {
             // @State var txt = "Ola Mundo" <- Swift
 
             var txt by remember { mutableStateOf("Ola Mundo") } // <- colocar "Ola Mundo" no estado
+            val txtTF = remember { mutableStateOf("") }
+            var header = "Teste"
 
             Column(modifier = Modifier.padding(horizontal = 15.dp)) {
+
+                Text(header, fontSize = 25.sp, modifier = Modifier.padding(25.dp))
+
                 Text(
                     txt,
                     fontSize = 20.sp,
@@ -41,15 +47,29 @@ class MainActivity : ComponentActivity() {
                         .clickable {
                             Log.d("aula", "clik no txt: ")
                         }
+                )                                      // it -> value + alterações
+                TextField(value = txtTF.value, onValueChange = { novotxt ->
+                    txtTF.value = novotxt
+                    Log.d("Aula", novotxt)
+
+                    },
+                    placeholder = {
+
+                        Text("Nome: ")
+                    }
+
                 )
 
                 Button(onClick = {
                     // ação do btn
+                    if ( txtTF.value.length > 3) {
+                        Log.d("aula", "txt: $txt ")
 
-                    Log.d("aula", "txt: $txt ")
-                    txt = "Nova msg"
-                    Log.d("aula", "txt: $txt ")
+                        txt = txtTF.value
+                        txtTF.value = ""
 
+                        Log.d("aula", "txt: $txt ")
+                    }
                 }) {
                     // Lbl
                     Text("Btn 1")
