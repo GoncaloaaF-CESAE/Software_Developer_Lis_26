bbcfdedfa282637ba1a8202ff28c9474d8f4abb1
GoncaloaaF <goncaloaaf@gmail.com>
2026-04-01 12:25:26 +0100

    load api com retrofit sem MVVM

--- Droid/13_loadapi/app/src/main/java/pt/cesae/a13_loadapi/MainActivity.kt
+++ Droid/13_loadapi/app/src/main/java/pt/cesae/a13_loadapi/MainActivity.kt
@@ -12,6 +12,11 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +33,9 @@ class MainActivity : ComponentActivity() {
             ) {
 
                 Button(onClick = {
-                    Log.d("Aula", "Load All Cmt")
+                    Log.d("Aula", "inicio - Load All Cmt")
+                    loadAllComments()
+                    Log.d("Aula", "fim - Load All Cmt")
 
                 }) {
                     Text("Load all")
@@ -36,6 +43,7 @@ class MainActivity : ComponentActivity() {
 
                 Button(onClick = {
                     Log.d("Aula", "Load cmt id 1")
+                    loadComment(1)
 
                 }) {
                     Text("Load id 1")
@@ -44,7 +52,65 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    fun loadAllComments(){}
+    fun loadAllComments(){
+        // isLoading = True
+        Log.d("Aula","Inicio - FN All" )
 
-    fun loadComment(id: Int){}
+        RetrofitInstance.api.getComments().enqueue(object: Callback<List<Comment>>{
+
+            override fun onResponse(
+                call: Call<List<Comment>>,
+                response: Response<List<Comment>>) {
+
+               if (response.isSuccessful){
+                   Log.d("Aula","Inicio - FN All -onResponse " )
+                   response.body()?.let {
+                       for (cmt in it){
+                           Log.d("Aula", cmt.email)
+                       }
+                   }
+                   Log.d("Aula","Fim - FN All -onResponse " )
+               }else {
+                   Log.e("Aula", "resposta sem sucesso")
+               }
+                // isLoading = False
+            }
+
+            override fun onFailure(call: Call<List<Comment>>, t: Throwable) {
+                Log.e("Aula", "Erro: ${t.message}")
+                // isLoading = False
+            }
+
+        })
+
+        Log.d("Aula","Fim - FN All" )
+    }
+
+    fun loadComment(id: Int){
+        // isLoading = True
+
+        RetrofitInstance.api.getComment(id).enqueue(object: Callback<Comment>{
+
+            override fun onResponse(
+                call: Call<Comment>,
+                response: Response<Comment>) {
+
+                if (response.isSuccessful){
+                    response.body()?.let {
+                     Log.d("Aula", it.email)
+                    }
+
+                }else {
+                    Log.e("Aula", "resposta sem sucesso")
+                }
+                // isLoading = False
+            }
+
+            override fun onFailure(call: Call<Comment>, t: Throwable) {
+                Log.e("Aula", "Erro: ${t.message}")
+                // isLoading = False
+            }
+
+        })
+    }
 }
