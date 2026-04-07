837e153c34b25681f00026022deb504e00d686ed
GoncaloaaF <goncaloaaf@gmail.com>
2026-04-07 12:51:47 +0100

    demo 2  nav

--- Droid/15_nav1/app/src/main/java/pt/cesae/a15_nav1/MainActivity.kt
+++ Droid/15_nav1/app/src/main/java/pt/cesae/a15_nav1/MainActivity.kt
@@ -1,6 +1,8 @@
 package pt.cesae.a15_nav1
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,12 +14,18 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,9 +35,7 @@ class MainActivity : ComponentActivity() {
             Column(verticalArrangement = Arrangement.Center,
                 modifier = Modifier.fillMaxSize()
                     .padding(15.dp)) {
-
                 AppNav()
-
             }
         }
     }
@@ -63,11 +69,31 @@ fun AppNav(){
 
 @Composable
 fun HomeScreen(navController: NavController){
+    var ctx = LocalContext.current
+
+    var nome by remember { mutableStateOf("") }
     Column {
         Text("Conteúdo da 1ª página")
-        
+
+        TextField(value = nome,
+            onValueChange = {
+                nome = it
+            },
+            placeholder = {
+                Text("nome: ")
+            })
+
         Button(onClick = {
-            navController.navigate("detalhe/Novo_nome")
+            if (nome.length >= 3 ) {
+                navController.navigate("detalhe/$nome")
+            }else {
+
+                Toast.makeText(ctx,
+                    "o nome tem de ter 3 ou mais  de letras",
+                    Toast.LENGTH_LONG).show()
+                    Log.d("Aula", "o nome tem de ter 3 ou mais  de letras ")
+            }
+
         }) {
             Text("ir para Detalhes")
         }
